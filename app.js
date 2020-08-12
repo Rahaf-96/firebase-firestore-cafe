b52d9af353e1556cf3c3232facf722bde21960e5
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-08-12 14:15:34 +0300

    storing data into firestore

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const cafeList = document.querySelector('#cafe-list');
-
+const form = document.querySelector('#add-cafe-form');
 // create element and render cafe
 
 const renderCafe = (doc) => {
@@ -25,3 +25,14 @@ db.collection('cafes')
       renderCafe(doc);
     });
   });
+
+// storing data
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('cafes').add({
+    name: form.name.value,
+    city: form.city.value,
+  });
+  form.name.value = '';
+  form.city.value = '';
+});
